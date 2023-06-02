bb5f66cffff3b696063278aecc6f54458f41f614
Abhishek Shah <amitkumarshah499@gmail.com>
2023-06-02 15:23:45 +0530

    build

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Failed to connect to MongoDB:', err));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB:', err));
 
 // Middleware
 app.use(express.json());
@@ -20,17 +20,21 @@ app.use(express.json());
 // Routes
 const usersRoutes = require('./routes/users');
 const puzzlesRoutes = require('./routes/puzzles');
-
 app.use('/api/users', usersRoutes);
 app.use('/api/puzzles', puzzlesRoutes);
 
+// Default screen path
+app.get('/', (req, res) => {
+  res.send('Welcome to the PuzzleLuck backend screen!');
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
+  console.error(err);
+  res.status(500).json({ error: 'Internal server error' });
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
